6bdea9d3b299c57b2ef921fa78f2c8b0a0997481
marim99 <marim.khaled99@gmail.com>
2023-03-18 16:42:48 +0200

    update ui

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -7,16 +7,16 @@
 <form method="POST" action="{{ route('posts.store') }}">
     @csrf
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Title</label>
+        <label for="exampleFormControlInput1" class="form-label fs-2">Title</label>
         <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+        <label for="exampleFormControlTextarea1" class="form-label fs-3">Description</label>
         <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
 
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Post Creator</label>
+        <label for="exampleFormControlTextarea1" class="form-label fs-3">Post Creator</label>
         <select name="creator" class="form-control">
             <option value="Mariam">Mariam</option>
             <option value="Hager">Hager</option>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -8,15 +8,15 @@
     @csrf
     <input type="hidden" name="id" value={{ $post['id'] }} class="form-control" id="exampleFormControlInput1">
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Title</label>
+        <label for="exampleFormControlInput1" class="form-label fs-2">Title</label>
         <input type="text" name="title" value={{ $post['title'] }} class="form-control" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+        <label for="exampleFormControlTextarea1" class="form-label fs-3">Description</label>
         <textarea class="form-control" name="description" id="exampleFormControlTextarea1" value={{ $post['description'] }} rows="3">{{ $post['description'] }}</textarea>
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Post Creator</label>
+        <label for="exampleFormControlTextarea1" class="form-label fs-4">Post Creator</label>
         <select name="creator" class="form-control" value="{{ $post['posted_by'] }}">
         <option value="{{ $post['posted_by'] }}">{{ $post['posted_by'] }}</option>
             <option value="Mariam">Mariam</option>
@@ -25,7 +25,7 @@
             <option value="Radwaa">Radwaa</option>
         </select>
     </div>
-    <button type="submit" class="btn btn-success">Edit</button>
+    <button type="submit" class="btn btn-success self-end">Edit</button>
 </form>
 </div>
 @endsection
